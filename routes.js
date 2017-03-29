388f7227bd5d438c28005bf1dd5b90dadfc85511
Scott Smith <mail@ScottHSmith.com>
2017-03-29 19:40:16 +0000

    Significant modularisation.

---
+++ routes.js
@@ -0,0 +1,88 @@
+// IMPORTS
+const config  = require('./config');
+const utils   = require('./utilities');
+const express = require('express');
+const router  = express.Router();
+const fs	  = require('fs');
+
+// ROUTES
+router.use(function(req, res, next) {
+	utils.dataIntegrity(config.data_file);
+	next();
+});
+
+router.route('/responses')
+	.get(function(req, res) {
+		try {
+			const json = JSON.parse(fs.readFileSync(config.data_file));
+			res.status(200).json(json);
+		}
+		catch(err) {
+			console.log(err);
+			res.status(500).send(err);
+		}
+	})
+	
+	.post(function(req, res) {
+		
+		fs.readFile(config.data_file, 'utf8', function readFileCallback(err, data) {
+			
+			var input = req.body.response;
+			input = utils.sanitiseInput(input);
+			var timestamp = utils.getTimestamp();
+			
+			// console.log(isNonemptyResponse(input));
+			// console.log(isUniqueResponse(input, data));
+			
+			data = JSON.parse(data);
+			
+			if (utils.isNonemptyResponse(input) && utils.isUniqueResponse(input, data)) {
+				try {
+					data[timestamp] = input;
+					json = JSON.stringify(data);
+					fs.writeFile(config.data_file, json, 'utf8');
+					
+					response = {'status': 'Success. Response recorded.'};
+					response.response = input;
+					res.status(202).json(response);
+					
+					utils.postSlackWebhook(input, timestamp);
+				}
+				catch(err){
+					console.log(err);
+					res.status(500).send(err);
+				}
+			}
+			else {
+				response = {'status': 'Response is a duplicate or empty.'};
+				response.response = input;
+				res.status(200).json(response);
+			}
+		});
+	});
+	
+router.route('/delete_response/:timestamp')
+	.all(function(req, res, next) {
+		var timestamp = req.params.timestamp;
+		
+		fs.readFile(config.data_file, 'utf8', function readFileCallback(err, data) {
+			data = JSON.parse(data);
+			
+			try {
+				delete data[timestamp];
+				json = JSON.stringify(data);
+				fs.writeFile(config.data_file, json, 'utf8');
+				
+				response = {'status': 'Success. Response deleted.'};
+				res.status(200).json(response);
+			}
+			catch(err){
+				res.status(500).send(err);
+			}
+		});
+	});
+
+// Actually use these routes.
+module.exports = function(app, config) {
+	app.use(`/v${config.api_version}`, router);
+}
---
+++ utilities.js
@@ -0,0 +1,119 @@
+const config  = require('./config');
+const fs      = require('fs');
+const request = require('request');
+
+var dataIntegrity = function(file_name) {
+	testExists(file_name);
+	testValidJSON(file_name);
+}
+
+var testExists = function(file_name) {
+	fs.open(file_name, 'r', (err, fd) => {
+		if (err) {
+			if (err.code === 'ENOENT') {
+				console.log('Data file does not exist.');
+				createDataFile(file_name);
+				return
+			}
+			throw err
+		}
+		return
+	});
+}
+
+var testValidJSON = function(file_name) {
+	fs.readFile(file_name, 'utf8', function readFileCallback(err, data) {
+		if (isJSON(data)) {
+			return
+		}
+		else {
+			console.log('Data invalid.');
+			createDataFile(file_name);
+			return
+		}
+	});
+}
+
+function isJSON(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
+
+var createDataFile = function(file_name) {
+	var obj = {}
+	var json = JSON.stringify(obj);
+	fs.writeFile(file_name, json, 'utf8');
+	console.log('Data file regenerated.');
+}
+
+var getTimestamp = function() {
+	return Date.now();
+}
+
+var isNonemptyResponse = function(input) {
+	if (typeof input == 'undefined') {
+		return false;
+	}
+	if (input.length == 0) {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+var isUniqueResponse = function(input, data) {
+	input = input.toLowerCase();
+	
+	for (var timestamp in data) {
+		// console.log('data.' + timestamp, '=', data[timestamp]);
+		if (data[timestamp].toLowerCase() == input) {
+			return false
+		}
+	}
+	return true
+}
+
+var sanitiseInput = function(input) {
+	input = input.trim();
+	input = input.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,''); // Remove puncuation
+	input = input.replace(/(<([^>]+)>)/ig,''); // Remove HTML tags
+	
+	return input
+}
+
+var postSlackWebhook = function(response, timestamp) {
+	var hook_url = 'https://hooks.slack.com/services/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'
+	var delete_url = config.baseURL + `api/v${config.api_version}/delete_response/` + timestamp
+	var payload = {
+		'attachments': [{
+			'fallback': response,
+			'title': response,
+			'text': `<${delete_url}|Delete>`,
+			'color': '#167EDA'
+		}]}
+	
+	request({
+		method: 'POST',
+	    url: hook_url,
+	    json: payload
+    },
+    function (error, response, body) {
+	    if (error) {
+			return console.error('Upload failed:', error);
+		}
+    });
+}
+
+module.exports = {
+	dataIntegrity: dataIntegrity,
+	getTimestamp: getTimestamp,
+	isNonemptyResponse: isNonemptyResponse,
+	isUniqueResponse: isUniqueResponse,
+	sanitiseInput: sanitiseInput,
+	postSlackWebhook: postSlackWebhook
+}

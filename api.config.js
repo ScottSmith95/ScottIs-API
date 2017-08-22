d97c68ef36264b8965720c74923710bfa4350bbd
Scott Smith <mail@ScottHSmith.com>
2017-08-22 18:23:49 +0000

    Updated config file formatting.

--- api.config.js
+++ api.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-  /**
-   * Application configuration section
-   * http://pm2.keymetrics.io/docs/usage/application-declaration/
-   */
-  apps : [
-    {
-      name   : 'ScottSmith.is API',
-      script : 'api.js',
-      cwd    : '/var/www/scottsmith.is/api/',
-      watch  : true,
-      ignore_watch: [".git", "data.json", "node_modules"],
-      env : {
-	    'PORT': 2370
-      },
-      env_development: {
-	    "BASE_URL": 'http://dev.scottsmith.is'
-      }
-    }
-  ]
+/**
+ * Application configuration section
+ * http://pm2.keymetrics.io/docs/usage/application-declaration/
+ */
+apps : [
+	{
+		name   : 'ScottSmith.is API',
+		script : 'api.js',
+		cwd    : '/var/www/scottsmith.is/api/',
+		watch  : true,
+		ignore_watch: [".git", "data.json", "node_modules"],
+		env: {
+			'PORT': 2370
+		},
+		env_development: {
+			"BASE_URL": 'http://dev.scottsmith.is'
+		}
+	}
+]
 };

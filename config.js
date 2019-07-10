853a5468ec9a38042f47360e230da69f1c7c013b
Scott Smith <mail@ScottHSmith.com>
2019-07-10 08:08:38 -0700

    move Slack webhook URL to env variable

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ config.base_url = process.env.BASE_URL || 'https://api.scottsmith.is/';
 config.port = process.env.PORT || 8080;
 config.api_version = `${ semver.major( api_semver ) }.${ semver.minor( api_semver ) }`;
 config.data_file = process.env.DATA_FILE || 'data.json';
+config.slackWebhookURL = process.env.SLACK_WEBHOOK;
 config.banned = require( './bans' );
 
 module.exports = config;
